dadfdca6daad3fd3a8354c328def879e11e6f1d0
D.N. Ceglia <d.n.ceglia@hotmail.com>
2017-02-21 19:15:58 -0500

    Test
    Test

--- Code/Project deuce/Project deuce/Person.cs
+++ Code/Project deuce/Project deuce/Person.cs
@@ -3,20 +3,56 @@
     class Person
     {
 
-        public int age;
-        public string firstName;
-        public string lastName;
-        public Person spouse;
+        public string FirstName;
+        public string LastName;
+        public int Age;
+        public Person Spouse;
+        public static int AgeCount;
+        public static int SumOfAllAges;
 
-        public string getFullName()
+        public string GetFullName()
         {
-
+            return this.FirstName + " " + this.LastName;
         }
 
-        public string printNameAndAge()
+        public void PrintNameAndAge()
         {
-            return this.firstName + this.lastName + " (" + // Person.age + ")";
+            System.Console.WriteLine(GetFullName() + " is " + this.Age + " years old.");
         }
 
+        public void StateYourNameAndAge()
+        {
+            System.Console.WriteLine("What is your first name? ");
+            this.FirstName = System.Console.ReadLine();
+
+            System.Console.WriteLine("What is your last name? ");
+            this.LastName = System.Console.ReadLine();
+
+            System.Console.WriteLine("How old are you? ");
+            this.Age = int.Parse (System.Console.ReadLine());
+        }
+
+        public void StateYourMaritalStatus()
+        {
+            System.Console.WriteLine("What is your spouse's first name if you're married? ");
+            this.Spouse.FirstName = System.Console.ReadLine();
+            this.Spouse.LastName = this.LastName;
+
+            System.Console.WriteLine("How old are they? ");
+            this.Spouse.Age = int.Parse(System.Console.ReadLine());
+
+            Person.AgeCount++;
+            Person.SumOfAllAges += Age;
+        }
+
+        public static double AgeAverage()
+        {
+            return Person.SumOfAllAges / Person.AgeCount;
+        }
+
+        public void PrintAverageAge()
+        {
+            System.Console.WriteLine("Your average age is " + AgeAverage() + " years old.");
+        }
     }
 }
--- Code/Project deuce/Project deuce/Program.cs
+++ Code/Project deuce/Project deuce/Program.cs
@@ -5,70 +5,25 @@
         static void Main(string[] args)
         {
 
-            Person somePerson = new Person();
-
-            somePerson.firstName = "John";
-            somePerson.lastName = "Doe";
-            somePerson.age = 34;
-            // somePerson.spouse = ;           // needs completed
-
-            somePerson.firstName = "Jane";
-            somePerson.lastName = "Doe";
-            somePerson.age = 22;
-            // somePerson.spouse = ;           // needs completed
-
-            somePerson.firstName = "David";
-            somePerson.lastName = "Smith";
-            somePerson.age = 76;
-            // somePerson.spouse = ;           // needs completed
-
-            somePerson.firstName = "Dora";
-            somePerson.lastName = "Smith";
-            somePerson.age = 64;
-            // somePerson.spouse = ;           // needs completed
-
-            ////////////////////////////////   option 2   //////////////////////////////////////////
-
             Person p1 = new Person();
-
-            System.Console.Write("What is your first name? ");
-            p1.firstName = "John";                              // do you declare variables for age, name etc. like this
-            // spousal question
-            System.Console.Write("What is your last name? ");
-            p1.lastName = System.Console.ReadLine();            // or like this
-            System.Console.Write("How old are you? ");
-            p1.age = 34;
-            System.Console.Write("What is your spouse's first name? ");
-            // p1.spouse = xxxxx;
-
+            p1.Spouse = new Person();
+            p1.StateYourNameAndAge();
+            p1.StateYourMaritalStatus(); 
 
             Person p2 = new Person();
-            p2.firstName = "Jane";
-            p2.lastName = "Doe";
-            p2.age = 22;
-            // p2.spouse = xxx;
+            p2.Spouse = new Person();
+            p2.StateYourNameAndAge();
+            p2.StateYourMaritalStatus();
 
-            // System.Console.WriteLine(p1.StateYourNameAndAge()); 
-            // System.Console.WriteLine(p2.StateYourNameAndAge());
+            p1.PrintNameAndAge();
+            p1.Spouse.PrintNameAndAge();
+            p2.PrintNameAndAge();
+            p2.Spouse.PrintNameAndAge();
+            // PrintAverageAge();
+            // System.Console.WriteLine("Your average age is " + Person.AgeAverage() + " years old.");
 
-            // Average age formula and output ..... here.....
-
-            // System.Threading.Thread.Sleep(5000);
-
-            ///////////////////////////////////          or          /////////////////////////////////
-
-            // System.Console.WriteLine("Press any key to continue...");
-            // System.Console.ReadKey();
-
-
-
-            //////////////////////////////   user input    ///////////////////////////////////////////
-
-            System.Console.Write("What is your name? ");
-            System.Console.Write("How old are you? ");
-            System.Console.Write("What is your marital status? ");
-            System.Console.Write("What is the first name of your spouse? ");
-            System.Console.Write("What is the age of your spouse? ");
+            System.Console.WriteLine("Press any key to continue...");
+            System.Console.ReadKey();
 
         }
     }
